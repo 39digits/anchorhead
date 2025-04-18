96c546d9d244428e381ce2f88c4d256a252efea8
Christopher Hamilton <christopher@39digits.com>
2025-04-18 17:05:21 +0100

    Remove key duplication in 1Password installer

--- install/desktop/app-1password.sh
+++ install/desktop/app-1password.sh
@@ -15,14 +15,6 @@ curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sud
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
 
-
-sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
-curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | \
-sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
-sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
-curl -sS https://downloads.1password.com/linux/keys/1password.asc | \
-sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-
 # Install 1Password & 1password-cli
 sudo apt update && sudo apt install -y 1password 1password-cli
 
